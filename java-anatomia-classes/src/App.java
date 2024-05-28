e8a7b5a6369f277b7c2417753589705351eac803
rewerp <rewer_p@hotmail.com>
2024-05-28 14:52:02 -0300

    Adicionado os arquivos iniciais de estudo do bootcamp

---
+++ java-anatomia-classes/src/App.java
@@ -0,0 +1,9 @@
+import edu.rewer.anatomiaclasses.*;
+
+public class App {
+  public static void main (String[] args) {
+    System.out.println("Olá mundo!");
+
+    BoletimEstudantil.main(args);
+  }
+}
---
+++ java-anatomia-classes/src/edu/rewer/anatomiaclasses/MinhaClasse.java
@@ -0,0 +1,16 @@
+package edu.rewer.anatomiaclasses;
+
+public class MinhaClasse {
+  public static void main (String[] args) {
+    String primeiroNome = "Rewer";
+    String segundoNome = "Pinheiro";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+  }
+
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
+  }
+}
